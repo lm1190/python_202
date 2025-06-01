a6e890fa81390e5fd727be108e7f58a47865922f
Lucas Graham Morrison <lm1190@scarletmail.rutgers.edu>
2025-06-01 22:41:44 +0000

    Initial commit

---
+++ maths.py
@@ -0,0 +1,51 @@
+num1_entered = int(input("Enter your first number: "))
+num2_entered = int(input("Enter your second number: "))
+
+sum_nums = num1_entered + num2_entered
+
+print(sum_nums)
+
+
+
+num1_entered = int(input("Enter your first number: "))
+num2_entered = int(input("Enter your second number: "))
+
+sum_nums = num1_entered - num2_entered
+
+print(sum_nums)
+
+
+
+num1_entered = int(input("Enter your first number: "))
+num2_entered = int(input("Enter your second number: "))
+
+sum_nums = num1_entered * num2_entered
+
+print(sum_nums)
+
+
+
+num1_entered = int(input("Enter your first number: "))
+num2_entered = int(input("Enter your second number: "))
+
+sum_nums = num1_entered / num2_entered
+
+print(sum_nums)
+
+
+
+num1_entered = int(input("Enter your first number: "))
+num2_entered = int(input("Enter your second number: "))
+
+sum_nums = num1_entered % num2_entered
+
+print(sum_nums)
+
+
+
+num1_entered = int(input("Enter your first number: "))
+num2_entered = int(input("Enter your second number: "))
+
+sum_nums = num1_entered ** num2_entered
+
+print(sum_nums)
